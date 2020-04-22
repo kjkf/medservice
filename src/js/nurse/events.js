7d9bbce34f42e1347db65f53b41537aa895269dd
Lola <yakjkf@gmail.com>
2020-04-22 14:42:23 +0600

    .layer set active

--- src/js/nurse/events.js
+++ src/js/nurse/events.js
@@ -1,16 +1,39 @@
 ;(function () {
     "use strict";
+    const clock = document.querySelector('.datetime');
+    if (clock) {
+        var calendar = new CalendarClock('.datetime');
+        var interval = setInterval(function () {
+            return calendar.update();
+        }, 1000);
+    }
 
-    var calendar = new CalendarClock('.datetime');
-    var interval = setInterval(function () {
-        return calendar.update();
-    }, 1000);
+    const tableWrapper = document.querySelector('.table_wrapper');
+    if (tableWrapper) {
+        var instance = OverlayScrollbars(tableWrapper, { });
 
-    const table = document.querySelector('.scrolledBody');
-    var instance = OverlayScrollbars(document.querySelector('.table_wrapper'), { });
-    console.log( $('#nurse_table'));
-    $('#nurse_table').floatThead({
-        position: 'fixed'
+        $('#nurse_table').floatThead({
+            position: 'fixed'
+        });
+        $('#nurse_table').floatThead('reflow');
+    }
+
+    const layers = document.querySelectorAll('.layer');
+
+    layers.forEach(layer => {
+        layer.addEventListener('click', e => setActive(layer))
     });
-    $('#nurse_table').floatThead('reflow');
+
+
+    function setActive(item) {
+        if (item.classList.contains('active')) {
+            item.classList.remove('active');
+            return false;
+        }
+
+        var parent = item.parentNode;
+        var activeItem = parent.querySelector('.active');
+        if (activeItem) activeItem.classList.remove('active');
+        item.classList.add('active');
+    }
 })();
