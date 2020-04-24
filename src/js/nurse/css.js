5f7111616479f01f859cb41444e06cc7ae7687a3
a_umut <askar.umut.kal@gmail.com>
2020-04-24 22:49:50 +0600

    Высота container для планшет;--add

--- src/js/nurse/css.js
+++ src/js/nurse/css.js
@@ -6,9 +6,8 @@ var isSafari = (currentUserAgent.indexOf('safari') > -1 && currentUserAgent.inde
 var winowHeight = window.innerHeight;
 var winowWidth = window.innerWidth;
 var container = document.querySelector(".container");
-container.style.height = winowHeight - (parseInt(container_pt) + parseInt(container_pb)) + "px";
+
 if (isSafari) {
     container.style.height = winowHeight;
-    container.style.background = 'green';
 }
 
