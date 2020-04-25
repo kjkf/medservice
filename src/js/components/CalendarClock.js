1cf3803b8030a0a23347b818d932c3ad3ff02b36
Lola <yakjkf@gmail.com>
2020-04-25 10:02:03 +0600

    day of week in mobile date

--- src/js/components/CalendarClock.js
+++ src/js/components/CalendarClock.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']; // Класс для даты с часами
+var days = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'];
 
 var CalendarClock = /*#__PURE__*/function () {
     // конструктор класса; инициализация значений
@@ -10,11 +11,13 @@ var CalendarClock = /*#__PURE__*/function () {
         this.timeDiv = this.$el.querySelector('.time-hm');
         this.secondsDiv = this.$el.querySelector('.seconds');
         this.dateDiv = this.$el.querySelector('.date-md');
+        this.dayDiv =  this.$el.querySelector('.date-dn');
         var dateStr = months[this.date.getMonth()] + ", " + this.date.getDate();
         this.dateDiv.textContent = dateStr;
         var hourStr = (this.date.getHours() < 10 ? '0' + this.date.getHours() : this.date.getHours()) + ":" + (this.date.getMinutes() < 10 ? '0' + this.date.getMinutes() : this.date.getMinutes());
         this.timeDiv.textContent = hourStr;
         this.secondsDiv.textContent = this.date.getSeconds() < 10 ? '0' + this.date.getSeconds() : this.date.getSeconds();
+        if (this.dayDiv) this.dayDiv.innerHTML = days[this.date.getDay()] + '<span>,</span>';
     } // метод, который обновляет значения на странице
 
 
@@ -27,6 +30,7 @@ var CalendarClock = /*#__PURE__*/function () {
         var hourStr = (this.date.getHours() < 10 ? '0' + this.date.getHours() : this.date.getHours()) + ":" + (this.date.getMinutes() < 10 ? '0' + this.date.getMinutes() : this.date.getMinutes());
         this.timeDiv.textContent = hourStr;
         this.secondsDiv.textContent = this.date.getSeconds() < 10 ? '0' + this.date.getSeconds() : this.date.getSeconds();
+        if (this.dayDiv) this.dayDiv.innerHTML = days[this.date.getDay()] + '<span>,</span>';
     };
 
     return CalendarClock;
